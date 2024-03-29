d8f43f01b85202b34ef717c5f0b2515e245da5b5
Lzzzt <liuzitao0123@gmail.com>
2024-03-29 15:30:25 +0800

    split read record and write record

--- benches/test.rs
+++ benches/test.rs
@@ -21,32 +21,21 @@ fn main() {
     let temp_dir = tempfile::tempdir().unwrap();
     let engine = Arc::new(open(temp_dir.path().to_path_buf()));
 
-    let key = Sentence(32..64);
-    let value = Sentence((3 << 10)..(5 << 10));
-
-    let mut insert_keys = Vec::with_capacity(160000);
-
-    for _ in 0..160000 {
-        let k = key.fake::<String>();
-        assert!(engine.put(k.clone(), value.fake::<String>()).is_ok());
-        insert_keys.push(k)
-    }
-
-    let insert_keys = Arc::new(insert_keys);
-
     let mut handlers = vec![];
 
     for i in 0..16 {
         let eng = Arc::clone(&engine);
-        let insert_keys = Arc::clone(&insert_keys);
 
         handlers.push(thread::spawn(move || {
+            let key = Sentence(32..64);
+            let value = Sentence((3 << 10)..(5 << 10));
             let mut costs = vec![];
-            for _ in i..(i + 1) * 10000 {
-                let key = &insert_keys[random::<usize>() % 160000];
 
+            for _ in i..(i + 1) * 2000 {
+                let k = key.fake::<String>();
+                let v = value.fake::<String>();
                 let start = Instant::now();
-                assert!(eng.get(key).is_ok());
+                assert!(eng.put(k, v).is_ok());
                 costs.push(start.elapsed().as_nanos());
             }
             costs
