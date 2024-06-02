a6649dad125074f0250f99d11906d3d56c0138db
Lzzzt <liuzitao0123@gmail.com>
2024-06-02 13:23:39 +0800

    update basic usage

--- core/examples/basic_usage.rs
+++ core/examples/basic_usage.rs
@@ -4,13 +4,15 @@ use bitcask_rs_core::{config::Config, db::Engine};
 
 fn main() -> Result<(), Box<dyn Error>> {
     let db = Engine::open(Config {
-        db_path: "/tmp/222/".into(),
+        db_path: "/tmp/bitcask/".into(),
         ..Default::default()
     })?;
 
-    db.put("111", "222")?;
+    db.put("foo", "bar")?;
 
     println!("{}", String::from_utf8(db.get("111")?)?);
+
+    db.del("foo")?;
 
     Ok(())
 }
